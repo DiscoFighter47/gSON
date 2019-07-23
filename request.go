d316a980cc5b47ae37813ec29adcc02ecf15c4da
Zahid Al Tair <mdzahidh119@gmail.com>
2019-07-24 00:09:20 +0600

    added request response helper

---
+++ request.go
@@ -0,0 +1,12 @@
+package gson
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// ParseBody ...
+func ParseBody(r *http.Request, v interface{}) error {
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(v)
+}
---
+++ response.go
@@ -0,0 +1,27 @@
+package gson
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Object ...
+type Object map[string]interface{}
+
+// Response ...
+type Response struct {
+	Code int
+	Data interface{} `json:"data,omitempty"`
+}
+
+// ServeJSON ...
+func (res *Response) ServeJSON(w http.ResponseWriter) {
+	if res.Code == 0 {
+		res.Code = http.StatusOK
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(res.Code)
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		panic(err)
+	}
+}
